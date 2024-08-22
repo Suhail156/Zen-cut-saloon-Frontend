af11184c243b61b268111ebd1277485b452f76f1
muhammad suhail <suhailpk539@gmail.com>
2024-08-22 15:22:26 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mui/styles']
-  }
+    include: ['@mui/styles'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
 });
+
